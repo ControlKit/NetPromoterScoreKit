b16008c63b5ed62d4649016c62d64a41e604b5d4
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2025-10-17 00:35:24 +0330

    Set some field to optional

--- Sources/NetPromoterScoreKit/Service/NPSResponse.swift
+++ Sources/NetPromoterScoreKit/Service/NPSResponse.swift
@@ -14,14 +14,14 @@ public struct NPSModel: Codable {
     public let id: String
     public let name: String
     public let title: LocalString
-    public let description: LocalString
-    public let recommend: LocalString
+    public let description: LocalString?
+    public let recommend: LocalString?
     public let icon: String?
-    public let min_title: LocalString
-    public let max_title: LocalString
-    public let input_label: LocalString
-    public let submit_button_title: LocalString
-    public let cancel_button_title: LocalString
+    public let min_title: LocalString?
+    public let max_title: LocalString?
+    public let input_label: LocalString?
+    public let submit_button_title: LocalString?
+    public let cancel_button_title: LocalString?
     public let type: String
     public let created_at: String?
 }
